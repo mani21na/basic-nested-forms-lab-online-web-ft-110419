4cc8a3d73dd579de7381a1d8409216ddee4616e7
Hanna Lee <mani21na@gmail.com>
2020-02-14 11:15:29 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,6 +23,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def update
+    Recipe.update(recipe_params)
+  end
+
   private
 
   def recipe_params
